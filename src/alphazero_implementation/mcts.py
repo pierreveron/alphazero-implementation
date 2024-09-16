cc0b284c2f6f4b4c17bde551fcc49f82c04f7bff
Pierre Véron <pierre.veron@outlook.fr>
2024-09-16 11:35:27 +0200

    refactor: add type annotation to mcts

--- src/alphazero_implementation/mcts.py
+++ src/alphazero_implementation/mcts.py
@@ -1,25 +1,28 @@
 import math
+from typing import List, Optional, TypeVar
+
+T = TypeVar("T")
 
 
 class Node:
-    def __init__(self, state, parent=None):
-        self.state = state
-        self.parent = parent
-        self.children = []
-        self.visits = 0
-        self.value = 0
+    def __init__(self, state: T, parent: Optional["Node"] = None):
+        self.state: T = state
+        self.parent: Optional["Node"] = parent
+        self.children: List["Node"] = []
+        self.visits: int = 0
+        self.value: float = 0
 
 
 class MCTS:
-    def __init__(self, exploration_weight=1.4):
-        self.exploration_weight = exploration_weight
+    def __init__(self, exploration_weight: float = 1.4):
+        self.exploration_weight: float = exploration_weight
 
-    def choose(self, node):
+    def choose(self, node: Node) -> Node:
         if not node.children:
             return self.expand(node)
         return max(node.children, key=self.uct_score)
 
-    def expand(self, node):
+    def expand(self, node: Node) -> Node:
         tried_children = {child.state for child in node.children}
         new_state = node.state.find_random_child()
         while new_state in tried_children:
@@ -28,19 +31,19 @@ class MCTS:
         node.children.append(child)
         return child
 
-    def simulate(self, node):
+    def simulate(self, node: Node) -> float:
         inplay = node.state
         while not inplay.is_terminal():
             inplay = inplay.find_random_child()
         return inplay.reward()
 
-    def backpropagate(self, node, reward):
+    def backpropagate(self, node: Node, reward: float) -> None:
         while node is not None:
             node.visits += 1
             node.value += reward
             node = node.parent
 
-    def uct_score(self, child):
+    def uct_score(self, child: Node) -> float:
         parent_visits = child.parent.visits
         if child.visits == 0:
             return float("inf")
@@ -48,7 +51,7 @@ class MCTS:
             math.log(parent_visits) / child.visits
         )
 
-    def search(self, initial_state, n_simulations):
+    def search(self, initial_state: T, n_simulations: int) -> Node:
         root = Node(initial_state)
         for _ in range(n_simulations):
             leaf = self.choose(root)
