48197c9dee615a3ac743f9ed5f3a95bb327067ab
Pierre Véron <pierre.veron@outlook.fr>
2024-11-19 14:39:07 +0100

    feat: store episodes in buffer

--- src/alphazero_implementation/alphazero/datamodule.py
+++ src/alphazero_implementation/alphazero/datamodule.py
@@ -6,33 +6,31 @@ import lightning as L
 import torch
 from torch.utils.data import DataLoader
 
-from alphazero_implementation.alphazero.types import Sample
+from alphazero_implementation.alphazero.types import Episode, Sample
 from alphazero_implementation.mcts.agent import MCTSAgent
 from alphazero_implementation.models.model import Model
 
 
 class EpisodeGenerator(threading.Thread):
-    def __init__(self, agent: MCTSAgent, buffer: deque[Sample]):
+    def __init__(self, agent: MCTSAgent, buffer: deque[Episode]):
         super().__init__(daemon=True)
         self.agent = agent
         self.buffer = buffer
         self.stop_event = threading.Event()
         self.lock = threading.Lock()
-        self.samples_added = 0
+        self.episodes_added = 0
 
-    def get_samples_added(self) -> int:
+    def get_episodes_added(self) -> int:
         with self.lock:
-            return self.samples_added
+            return self.episodes_added
 
     def run(self):
         while not self.stop_event.is_set():
             episodes = self.agent.generate_episodes()
             for episode in episodes:
-                samples = episode.samples
-                # print(f"Adding {len(episode.samples)} samples to the buffer")
                 with self.lock:
-                    self.buffer.extend(samples)
-                    self.samples_added += len(samples)
+                    self.buffer.append(episode)
+                    self.episodes_added += 1
 
     def stop(self):
         self.stop_event.set()
@@ -47,8 +45,8 @@ class AlphaZeroDataModule(L.LightningDataModule):
         shuffle: bool = True,
         num_workers: int = 8,
         persistent_workers: bool = True,
-        buffer_size: int = 50000,
-        min_new_data: int = 100,
+        buffer_size: int = 5000,
+        min_new_episodes: int = 100,
     ):
         super().__init__()
         self.model = model
@@ -58,10 +56,10 @@ class AlphaZeroDataModule(L.LightningDataModule):
         self.num_workers = num_workers
         self.persistent_workers = persistent_workers
         self.buffer_size = buffer_size
-        self.buffer: deque[Sample] = deque(maxlen=buffer_size)
+        self.buffer: deque[Episode] = deque(maxlen=buffer_size)
         self.episode_generator = EpisodeGenerator(self.agent, self.buffer)
-        self.min_new_data = min_new_data
-        self.last_samples_added = 0
+        self.min_new_episodes = min_new_episodes
+        self.last_episodes_added = 0
 
     def setup(self, stage: str):
         if stage == "fit":
@@ -74,24 +72,28 @@ class AlphaZeroDataModule(L.LightningDataModule):
         # Wait until we have min_new_data new samples
         start_time = time.time()
         while (
-            self.episode_generator.get_samples_added() - self.last_samples_added
-        ) < self.min_new_data:
+            self.episode_generator.get_episodes_added() - self.last_episodes_added
+        ) < self.min_new_episodes:
             # print(
             #     f"Waiting for {self.min_new_data} new samples, {self.episode_generator.get_samples_added() - self.last_samples_added} already added in {time.time() - start_time:.2f} seconds"
             # )
             time.sleep(0.1)  # Sleep for a short time to avoid busy waiting
 
         waited_time = time.time() - start_time
-        samples_added = self.episode_generator.get_samples_added()
-        new_samples = samples_added - self.last_samples_added
+        episodes_added = self.episode_generator.get_episodes_added()
+        new_episodes = episodes_added - self.last_episodes_added
 
-        print(f"Got {new_samples} new samples in {waited_time:.2f} seconds")
+        print(f"Got {new_episodes} new episodes in {waited_time:.2f} seconds")
 
-        self.last_samples_added = samples_added
+        self.last_episodes_added = episodes_added
 
-        states = [sample.state for sample in self.buffer]
-        policies = [sample.policy for sample in self.buffer]
-        values = [sample.value for sample in self.buffer]
+        all_samples: list[Sample] = []
+        for episode in self.buffer:
+            all_samples.extend(episode.samples)
+
+        states = [sample.state for sample in all_samples]
+        policies = [sample.policy for sample in all_samples]
+        values = [sample.value for sample in all_samples]
 
         dataset = self.model.format_dataset(states, policies, values)
 
