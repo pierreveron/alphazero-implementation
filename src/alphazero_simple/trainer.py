ebdc004553997f1c6d654cdb7d6f6a62e560e090
Pierre Véron <pierre.veron@outlook.fr>
2024-12-12 19:37:06 +0100

    feat: clean trainer and add logs

--- src/alphazero_simple/trainer.py
+++ src/alphazero_simple/trainer.py
@@ -1,22 +1,35 @@
 import os
-import numpy as np
 from random import shuffle
+from typing import Any
 
+import numpy as np
 import torch
-import torch.optim as optim
+from torch import optim
+from torch.utils.tensorboard.writer import SummaryWriter
 
-from monte_carlo_tree_search import MCTS
+from .connect4_game import Connect4Game
+from .connect4_model import Connect4Model
+from .monte_carlo_tree_search import MCTS
+
 
 class Trainer:
-
-    def __init__(self, game, model, args):
+    def __init__(
+        self,
+        game: Connect4Game,
+        model: Connect4Model,
+        device: torch.device,
+        args: dict[str, Any],
+    ):
         self.game = game
         self.model = model
+        self.device = device
         self.args = args
         self.mcts = MCTS(self.game, self.model, self.args)
+        self.writer = SummaryWriter(
+            os.path.join("runs", args.get("exp_name", "default_run"))
+        )
 
-    def exceute_episode(self):
-
+    def exceute_episode(self) -> list[tuple[np.ndarray, np.ndarray, float]]:
         train_examples = []
         current_player = 1
         state = self.game.get_init_board()
@@ -35,31 +48,42 @@ class Trainer:
             train_examples.append((canonical_board, current_player, action_probs))
 
             action = root.select_action(temperature=0)
-            state, current_player = self.game.get_next_state(state, current_player, action)
+            state, current_player = self.game.get_next_state(
+                state, current_player, action
+            )
             reward = self.game.get_reward_for_player(state, current_player)
 
             if reward is not None:
                 ret = []
-                for hist_state, hist_current_player, hist_action_probs in train_examples:
+                for (
+                    hist_state,
+                    hist_current_player,
+                    hist_action_probs,
+                ) in train_examples:
                     # [Board, currentPlayer, actionProbabilities, Reward]
-                    ret.append((hist_state, hist_action_probs, reward * ((-1) ** (hist_current_player != current_player))))
+                    ret.append(
+                        (
+                            hist_state,
+                            hist_action_probs,
+                            reward * ((-1) ** (hist_current_player != current_player)),
+                        )
+                    )
 
                 return ret
 
     def learn(self):
-        for i in range(1, self.args['numIters'] + 1):
-
-            print("{}/{}".format(i, self.args['numIters']))
+        for i in range(1, self.args["numIters"] + 1):
+            print("{}/{}".format(i, self.args["numIters"]))
 
             train_examples = []
 
-            for eps in range(self.args['numEps']):
+            for eps in range(self.args["numEps"]):
                 iteration_train_examples = self.exceute_episode()
                 train_examples.extend(iteration_train_examples)
 
             shuffle(train_examples)
             self.train(train_examples)
-            filename = self.args['checkpoint_path']
+            filename = self.args["checkpoint_path"]
             self.save_checkpoint(folder=".", filename=filename)
 
     def train(self, examples):
@@ -67,22 +91,25 @@ class Trainer:
         pi_losses = []
         v_losses = []
 
-        for epoch in range(self.args['epochs']):
+        for epoch in range(self.args["epochs"]):
             self.model.train()
-
+            epoch_pi_losses = []
+            epoch_v_losses = []
             batch_idx = 0
 
-            while batch_idx < int(len(examples) / self.args['batch_size']):
-                sample_ids = np.random.randint(len(examples), size=self.args['batch_size'])
+            while batch_idx < int(len(examples) / self.args["batch_size"]):
+                sample_ids = np.random.randint(
+                    len(examples), size=self.args["batch_size"]
+                )
                 boards, pis, vs = list(zip(*[examples[i] for i in sample_ids]))
                 boards = torch.FloatTensor(np.array(boards).astype(np.float64))
                 target_pis = torch.FloatTensor(np.array(pis))
                 target_vs = torch.FloatTensor(np.array(vs).astype(np.float64))
 
                 # predict
-                boards = boards.contiguous().cuda()
-                target_pis = target_pis.contiguous().cuda()
-                target_vs = target_vs.contiguous().cuda()
+                boards = boards.contiguous().to(self.device)
+                target_pis = target_pis.contiguous().to(self.device)
+                target_vs = target_vs.contiguous().to(self.device)
 
                 # compute output
                 out_pi, out_v = self.model(boards)
@@ -92,6 +119,20 @@ class Trainer:
 
                 pi_losses.append(float(l_pi))
                 v_losses.append(float(l_v))
+                epoch_pi_losses.append(float(l_pi))
+                epoch_v_losses.append(float(l_v))
+
+                # Log batch losses
+                global_step = (
+                    epoch * int(len(examples) / self.args["batch_size"]) + batch_idx
+                )
+                self.writer.add_scalar(
+                    "Loss/train/policy_loss", float(l_pi), global_step
+                )
+                self.writer.add_scalar("Loss/train/value_loss", float(l_v), global_step)
+                self.writer.add_scalar(
+                    "Loss/train/total_loss", float(total_loss), global_step
+                )
 
                 optimizer.zero_grad()
                 total_loss.backward()
@@ -99,9 +140,24 @@ class Trainer:
 
                 batch_idx += 1
 
+            # Log epoch average losses
+            epoch_pi_loss = np.mean(epoch_pi_losses)
+            epoch_v_loss = np.mean(epoch_v_losses)
+            self.writer.add_scalar("Loss/epoch/policy_loss", epoch_pi_loss, epoch)
+            self.writer.add_scalar("Loss/epoch/value_loss", epoch_v_loss, epoch)
+
+            # Log example predictions vs targets
+            if epoch % 10 == 0:  # Log every 10 epochs to avoid too much data
+                self.writer.add_histogram(
+                    "Predictions/policy", out_pi[0].detach(), epoch
+                )
+                self.writer.add_histogram("Targets/policy", target_pis[0], epoch)
+                self.writer.add_histogram("Predictions/value", out_v.detach(), epoch)
+                self.writer.add_histogram("Targets/value", target_vs, epoch)
+
             print()
-            print("Policy Loss", np.mean(pi_losses))
-            print("Value Loss", np.mean(v_losses))
+            print("Policy Loss", epoch_pi_loss)
+            print("Value Loss", epoch_v_loss)
             print("Examples:")
             print(out_pi[0].detach())
             print(target_pis[0])
@@ -111,7 +167,7 @@ class Trainer:
         return loss.mean()
 
     def loss_v(self, targets, outputs):
-        loss = torch.sum((targets-outputs.view(-1))**2)/targets.size()[0]
+        loss = torch.sum((targets - outputs.view(-1)) ** 2) / targets.size()[0]
         return loss
 
     def save_checkpoint(self, folder, filename):
@@ -119,6 +175,13 @@ class Trainer:
             os.mkdir(folder)
 
         filepath = os.path.join(folder, filename)
-        torch.save({
-            'state_dict': self.model.state_dict(),
-        }, filepath)
+        torch.save(
+            {
+                "state_dict": self.model.state_dict(),
+            },
+            filepath,
+        )
+
+    def __del__(self):
+        # Close the tensorboard writer when the trainer is destroyed
+        self.writer.close()
