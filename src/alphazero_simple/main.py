afa38ddfa95f31ac2573f1cddd129c158c7d2a3a
Pierre Véron <pierre.veron@outlook.fr>
2024-12-12 19:36:03 +0100

    feat: use connect4 in main

--- src/alphazero_simple/main.py
+++ src/alphazero_simple/main.py
@@ -1,26 +1,26 @@
 import torch
 
-from game import Connect2Game
-from model import Connect2Model
-from trainer import Trainer
+from alphazero_simple.connect4_game import Connect4Game
+from alphazero_simple.connect4_model import Connect4Model
+from alphazero_simple.trainer import Trainer
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 args = {
-    'batch_size': 64,
-    'numIters': 500,                                # Total number of training iterations
-    'num_simulations': 100,                         # Total number of MCTS simulations to run when deciding on a move to play
-    'numEps': 100,                                  # Number of full games (episodes) to run during each iteration
-    'numItersForTrainExamplesHistory': 20,
-    'epochs': 2,                                    # Number of epochs of training per iteration
-    'checkpoint_path': 'latest.pth'                 # location to save latest set of weights
+    "batch_size": 64,
+    "numIters": 500,  # Total number of training iterations
+    "num_simulations": 25,  # Total number of MCTS simulations to run when deciding on a move to play
+    "numEps": 100,  # Number of full games (episodes) to run during each iteration
+    "numItersForTrainExamplesHistory": 20,
+    "epochs": 2,  # Number of epochs of training per iteration
+    "checkpoint_path": "latest.pth",  # location to save latest set of weights
 }
 
-game = Connect2Game()
+game = Connect4Game()
 board_size = game.get_board_size()
 action_size = game.get_action_size()
 
-model = Connect2Model(board_size, action_size, device)
+model = Connect4Model(board_size, action_size, device)
 
-trainer = Trainer(game, model, args)
+trainer = Trainer(game, model, device, args)
 trainer.learn()
