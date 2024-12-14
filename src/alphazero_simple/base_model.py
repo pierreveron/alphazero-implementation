b606436260496527b8feccd8f1259429bf9edb10
Pierre Véron <pierre.veron@outlook.fr>
2024-12-14 17:26:05 +0100

    refactor: clean BaseModel

--- src/alphazero_simple/base_model.py
+++ src/alphazero_simple/base_model.py
@@ -1,15 +1,10 @@
 from abc import ABC, abstractmethod
 
-import torch
 from numpy.typing import NDArray
 from torch import nn
 
 
 class BaseModel(nn.Module, ABC):
-    def __init__(self, device: torch.device):
-        super().__init__()
-        self.device = device
-
     @abstractmethod
     def predict(self, board: NDArray) -> tuple[NDArray, float]:
         """
